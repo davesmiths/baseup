e24f17b1856f4584e9433cf75b3f3212950b33e8
Dave Smith <da__smith@hotmail.com>
2017-10-10 14:17:53 +0100

    Change clearfix to use the latest version
    Change was in response to a Safari 11 issue causing columns to wrap around incorrectly. The cause was the display:table in the previous clearfix.
    
    Also updated the test and default CSS and JS

--- baseup.js
+++ baseup.js
@@ -8,7 +8,6 @@
 (function($){
 
     'use strict';
-
     // isIE
     /* github.com/davesmiths/isIE */var isIE=false,isIEmode;/*@cc_on isIE=@_jscript_version;@*/if(isIE!==false){if(isIE==5.8)isIE=8;else if(isIE==5.7 && window.XMLHttpRequest)isIE=7;else if(isIE==5.7 || isIE==5.6)isIE=6;else if(isIE<=5.5)isIE=5;isIEmode=isIE;if(document.documentMode)isIEmode=document.documentMode;}
 
@@ -52,11 +51,9 @@
             if (isIE == 6) {
                 this.each(function() {
 
-                    var $cols = $this = $(this)
-                        ,val
-                        ,valLength
-                    ;
+                    var $cols,$this,val,valLength;
 
+                    $cols = $this = $(this);
 
                     // Cols classes
                     if ($this.find('> .baseup-legacy-support-lay').length) {
@@ -76,7 +73,7 @@
                         // Get the last set widths class if more than one is set
                         for (i = 0; i < valLength; i++) {
                             val[i] = val[i].replace('widths', 'width');
-                            if (m = val[i].match(/([0-9]+)up$/)) {
+                            if ((m = val[i].match(/([0-9]+)up$/))) {
                                 val[i] = {bp: m[1], val: val[i]};
                             }
                             else {
@@ -86,10 +83,11 @@
                         }
 
                         $cols.find('> *').not('.clear').each(function() {
-                            var $this = $(this)
-                                ,className = $this.attr('class')
-                                ,bp
-                            ;
+                            var $this,className,bp;
+
+                            $this = $(this);
+                            className = $this.attr('class');
+
                             for (i = 0; i < valLength; i++) {
                                 bp = val[i].bp + 'up';
                                 if (val[i].bp === 0) {
@@ -101,22 +99,6 @@
                             }
                         });
                     }
-
-
-
-                    // Guts Full Width Friendly classes
-                    val = $this.attr('class').match(/\s?guts-fw[0-9a-z-]*/g);
-
-                    if (val) {
-                        // Get the last set widths class if more than one is set
-                        val = val[val.length - 1];
-                        val = val.replace('guts-fw', 'gut-left');
-
-                        $cols.find('> * > *').not(".clear, [class^='gut-left-'],[class*=' gut-left-']").each(function() {
-                            $(this).addClass(val);
-                        });
-                    }
-
 
                 });
 
@@ -132,6 +114,127 @@
         $('.lay, .lay-left, .lay-right, .lay-centered').baseUp({legacySupportLay:true});
         $('.cols').baseUp({legacySupportCols:true});
     });
+
+    // If flex is not supported, then we want to engage float positions
+    (function() {
+
+        var flexsupported,display;
+
+        flexsupported = false;
+
+        // document.documentElement.style.backgroundColor = 'red';
+
+        try {
+
+            display = document.documentElement.style.display;
+
+            document.documentElement.style.display = 'flex';
+            if (document.documentElement.style.display === 'flex') {
+                //supported
+                flexsupported = true;
+                // document.documentElement.style.backgroundColor = 'green';
+            }
+            else {
+                document.documentElement.style.display = '-webkit-flex';
+                if (document.documentElement.style.display === '-webkit-flex') {
+                    //supported
+                    flexsupported = true;
+                    // document.documentElement.style.backgroundColor = 'green';
+                }
+            }
+
+            document.documentElement.style.display = display;
+
+        }
+        // Catch required for some versions of IE
+        catch (e) {
+
+        }
+
+        if (!flexsupported) {
+            document.documentElement.className += ' baseup-noflex';
+        }
+
+    }());
+
+
+    // Provide means to create a sensible max-width for browsers that don't support media queries
+
+    // https://raw.githubusercontent.com/paulirish/matchMedia.js/master/matchMedia.js
+    /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
+
+    window.matchMedia || (window.matchMedia = function() {
+
+        // For browsers that support matchMedium api such as IE 9 and webkit
+        var styleMedia = (window.styleMedia || window.media);
+
+        // For those that don't support matchMedium
+        if (!styleMedia) {
+            var style       = document.createElement('style'),
+                script      = document.getElementsByTagName('script')[0],
+                info        = null;
+
+            style.type  = 'text/css';
+            style.id    = 'matchmediajs-test';
+
+            script.parentNode.insertBefore(style, script);
+
+            // 'style.currentStyle' is used by IE <= 8 and 'window.getComputedStyle' for all other browsers
+            info = ('getComputedStyle' in window) && window.getComputedStyle(style, null) || style.currentStyle;
+
+            styleMedia = {
+                matchMedium: function(media) {
+                    var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }';
+
+                    // 'style.styleSheet' is used by IE <= 8 and 'style.textContent' for all other browsers
+                    if (style.styleSheet) {
+                        style.styleSheet.cssText = text;
+                    } else {
+                        style.textContent = text;
+                    }
+
+                    // Test if media query is true or false
+                    return info.width === '1px';
+                }
+            };
+        }
+
+        return function(media) {
+            return {
+                matches: styleMedia.matchMedium(media || 'all'),
+                media: media || 'all'
+            };
+        };
+    }());
+
+    if (!window.matchMedia('(min-width:400px)').matches) {
+
+        (function() {
+
+            var bp,bps,i,clientWidth;
+
+            if (document.documentElement && document.documentElement.clientWidth) {
+
+                bps = [];
+                clientWidth = document.documentElement.clientWidth;
+
+                bps[0] = 0;bps[1] = 700;bps[2] = 940;bps[3] = 1180;
+
+                bp = 0;
+                for (i = 1; i < bps.length; i += 1) {
+                    if (bps[i] >= clientWidth) {
+                        break;
+                    }
+                    bp = i;
+                }
+
+                document.documentElement.className += ' baseup-legacy-bp-' + bp;
+
+            }
+
+        }());
+
+    }
 
 
 }(jQuery));
